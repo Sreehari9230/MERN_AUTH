3d6244ec8f971dc55591e39e0a42c98ea5be561f
Sreehari <sreeharimangalasseri528@gmail.com>
2025-09-23 19:05:12 +0530

    SendingWelcomeEmail

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -20,3 +20,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error in sending email verificatin: ${error}`);
     }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recepient = [{ email }]
+
+    try {
+        const response = await MailTrapClient.send({
+            from: sender,
+            to: recepient,
+            template_uuid: "32117fe5-e7d6-419f-8655-b2dc62dc1567",
+            template_variables: {
+                "company_info_name": "Mern Auth",
+                "name": name
+            }
+        })
+        console.log("Welcome Email Send Successfully", response);
+
+    } catch (error) {
+        console.log(`Error in sending  welcome email: ${error}`);
+        throw new Error(`Error in sending velcome email: ${error}`);
+
+    }
+}
