09d5e1da8117eadc664484f25876a4ca862f4738
Sreehari <sreeharimangalasseri528@gmail.com>
2025-09-25 21:44:20 +0530

    NothingMusch

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcryptjs from "bcryptjs";
 
 import { User } from "../models/user.model.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import {
+import { 
     sendPasswordResetEmail,
     sendResetSuccessEmail,
     sendVerificationEmail,
